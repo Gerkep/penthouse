adde75daefd81b86c2a4f9d08ba12e44bc14ffd8
Piotr Gerke <piotrg2003@gmail.com>
2022-11-04 07:32:45 +0100

    footer update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ea659c42a98c185"; }
+/******/ 		__webpack_require__.h = function() { return "1b03ece67c3c4ae4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
