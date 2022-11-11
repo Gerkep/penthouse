b496552251ea74193fa027eba950bb0dee18cd60
Piotr Gerke <piotrg2003@gmail.com>
2022-11-11 20:20:32 +0100

    next downgrade

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "76cb0ee761d0c436"; }
+/******/ 		__webpack_require__.h = function() { return "2ce1075ab7cc063f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
