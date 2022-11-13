72a2258ad93416e26b01c01ec97d17e3f679416b
Piotr Gerke <piotrg2003@gmail.com>
2022-11-13 12:08:51 +0100

    viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" /> 
         <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <MantineProvider>
           <NotificationsProvider zIndex={2077}>
